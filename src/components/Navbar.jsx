cbc8825c2e3f3d0354c18e2262f605b4f2d940b1
Khandaker Sojol Ahmed <dev.khandakersojol@gmail.com>
2025-10-22 22:52:00 +0600

    navbar done & all package install done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,69 @@
+import { NavLink } from "react-router";
+import logo from "../assets/logo.png";
+
+const Navbar = () => {
+  const navLinks = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/services">Services</NavLink>
+      </li>
+      <li>
+        <NavLink to="/my-profile">My Profile</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar py-4">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow text-[#706F6F] text-lg"
+          >
+            {navLinks}
+          </ul>
+        </div>
+        <a className="text-lg ">
+          <img className="max-w-30" src={logo} alt="Logo" />
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 text-[#706F6F] text-lg">
+          {navLinks}
+        </ul>
+      </div>
+      <div className="navbar-end gap-3">
+        <div className="avatar">
+          <div className="w-14 rounded-full">
+            <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" />
+          </div>
+        </div>
+        <button className="bg-[#ff3600] px-8 md:px-12 py-3 text-white font-semibold rounded-4xl text-xl cursor-pointer">
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../rootLayouts/RootLayout";
+import Home from "../pages/Home";
+import Services from "../pages/Services";
+import MyProfile from "../pages/MyProfile";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<RootLayout></RootLayout>,
+    children:[
+      {
+         path:"/",
+         element:<Home></Home>
+      },
+      {
+         path:"/services",
+         element:<Services></Services>
+      },
+      {
+         path:"/my-profile",
+         element:<MyProfile></MyProfile>
+      },
+    ],
+  },
+]);
+export default router;
